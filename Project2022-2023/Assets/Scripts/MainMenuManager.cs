141665a3010d38661f0ccc5eaba6e106d2ca3136
matoula27 <matou.lekka@gmail.com>
2023-01-19 15:53:58 +0200

    Main Menu Scene - Part 2

---
+++ Project2022-2023/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
