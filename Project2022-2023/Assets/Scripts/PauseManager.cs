c059dd9a1facbd880d72397461a2fa84761dd665
matoula27 <matou.lekka@gmail.com>
2023-01-21 15:14:18 +0200

    Pause Menu - Part 2

--- Project2022-2023/Assets/Scripts/PauseManager.cs
+++ Project2022-2023/Assets/Scripts/PauseManager.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PauseManager : MonoBehaviour
 {
     public GameObject pauseScreen;
+    public CinemachineBrain cinemachineBrain;
     public static bool onPause;
+    float previousTimeScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        previousTimeScale = Time.timeScale;
+
+        onPause = false;
+        pauseScreen.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.P))
         {
             if (!onPause)
             {
@@ -33,11 +39,20 @@ public class PauseManager : MonoBehaviour
     {
         onPause = true;
         pauseScreen.SetActive(onPause);
+
+        cinemachineBrain.enabled = !onPause;
+
+        previousTimeScale = Time.timeScale;
+        Time.timeScale = 0f;
     }
 
     void Unpause()
     {
         onPause = false;
         pauseScreen.SetActive(onPause);
+
+        cinemachineBrain.enabled = !onPause;
+
+        Time.timeScale = previousTimeScale;
     }
 }
